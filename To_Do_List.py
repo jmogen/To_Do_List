d0e22ec1853d9e4e4c8e92832b91ebf2d2d28993
Jack Mogen <jmogen@uwaterloo.ca>
2022-06-21 20:11:13 -0400

    Unique text for edit

--- To_Do_List.py
+++ To_Do_List.py
@@ -17,7 +17,8 @@ t = conn.cursor()
 #     status text
 # )""")
 
-t.execute("SELECT * FROM tasks")
+t.execute("SELECT * FROM tasks ORDER BY due_date DESC")
+#t.execute("INSERT INTO tasks VALUES ('Unique Task', 'Unique Text For Filler', '3', 'true')")
 items = t.fetchall() 
 
 i = 0
@@ -28,22 +29,38 @@ conn.commit()
 sg.theme('Black')  # please make your windows colorful
 
 col_actions = [[sg.Text('Due Date'),sg.Text('Status') ]]
-layout = [[sg.Text('UPCOMING'), sg.Button('Add'),sg.Button('TEST')],
+layout = [[sg.Text('UPCOMING'), sg.Button('Add')],
 [sg.Text('TASKS'), sg.Column(col_actions, element_justification='right')],
 [sg.Listbox(values=[f'{x[0]}: '+ f'{x[2]}   '+ f'{x[3]}' for x in items], size=(59,6))]]
 
-layout += [[sg.Text(f'{i+1}. '), sg.Text(f'{items[i][0]}'), sg.Text(f'{items[i][2]}'),sg.Text(f'{items[i][3]}'), sg.Checkbox('', default=items[i][3]=='true'), sg.Button('Edit', key=lambda: popup_prediction())] for i in range(0,2)]
+layout += [[sg.Text(f'{i+1}. '), sg.Text(f'{items[i][0]}'), sg.Text(f'{items[i][2]}'),sg.Text(f'{items[i][3]}'), sg.Checkbox('', default=items[i][3]=='true'), sg.Button('Edit', key=lambda: edit_prediction(items[i][0], items[i][1], items[i][2]))] for i in range(0,len(items))]
         
 
 window = sg.Window('TASKS', layout, resizable=True)
 
-def popup_prediction():
+
+def add_prediction():
 
     col_layout = [[sg.Button('Save')]]
     layout = [
         [sg.Text("Task:       "), sg.Input()],
         [sg.Text("Description:"), sg.Multiline(size=(None,5))],
         [sg.Text("Due Date:   "), sg.Input()],
+        [sg.Column(col_layout, expand_x=True, element_justification='right')],
+    ]
+    window = sg.Window("Prediction", layout, use_default_focus=False, finalize=True, modal=True)
+    #block_focus(window)
+    event, values = window.read()
+    window.close()
+    return None
+
+def edit_prediction(ta, de, dd):
+
+    col_layout = [[sg.Button('Save')]]
+    layout = [
+        [sg.Text("Task:       "), sg.Input(f'{ta}')],
+        [sg.Text("Description:"), sg.Multiline(f'{de}',size=(None,5))],
+        [sg.Text("Due Date:   "), sg.Input(f'{dd}')],
         [sg.Column(col_layout, expand_x=True, element_justification='right')],
     ]
     window = sg.Window("Prediction", layout, use_default_focus=False, finalize=True, modal=True)
@@ -60,6 +77,6 @@ while True:  # Event Loop
     if callable(event):
         event()
     elif event == 'Add':
-        popup_prediction()
+        add_prediction()
 
 window.close()
